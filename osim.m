50b574cd9fe994840220a65130330db524e35b64
Paul Apelt <lt.kempf@gmail.com>
2015-09-29 16:46:06 +1000

    Added avg over multiple runs

--- osim.m
+++ osim.m
@@ -1,23 +1,37 @@
-function [ m1, m2, m3, mtp ] = osim( tmax, til )
+function [ m1, m2, m3, mtp ] = osim( tmax, til, run )
 %OSIM Packaging optimization
 %   tmax - simulation time
 %   til - tiling in seconds
+%   run - number of runs
     awt = NaN(100,100,100);
     str = '';
     fprintf('\nSimulating...\t');
-    for r1 = 1:til:100
-        for r2 = 1:til:100
-            for r3 = 1:til:100
-                if(true)
+    % Sanity check
+    if (run < 1)
+        error('Incorrect number of runs.');
+    end
+    % Optimization loop
+    for n = 1:run
+        for r1 = 1:til:100
+            for r2 = 1:til:100
+                for r3 = 1:til:100
+                    % Progress output
                     rem = repmat('\b',1,length(str));
                     str = [num2str(r1) ':' num2str(r2) ':' num2str(r3)];
                     fprintf([rem str]);
+                    % Run the sim
+                    if (n==1)
+                        awt(r1,r2,r3) = asim(tmax, r1, r2, r3);
+                    else
+                        awt(r1,r2,r3) = awt(r1,r2,r3) + asim(tmax, r1, r2, r3);
+                    end
                 end
-                awt(r1,r2,r3) = asim(tmax, r1, r2, r3);
             end
         end
     end
-    
+    % Average the results
+    awt = awt ./ run;
+    % Calculate minimum
     [mtp,ind] = max(awt(:));
     [m1, m2, m3] = ind2sub(size(awt),ind);
     fprintf('\nMaximum output: %.2f\n\tm1\t%d\n\tm2\t%d\n\tm3\t%d\n',mtp,m1,m2,m3);
