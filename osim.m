d41aa33313664114fdea07a79c21e88d9882d3e9
Paul Apelt <lt.kempf@gmail.com>
2015-10-02 14:37:13 +1000

    Merged Graphics into osim

--- osim.m
+++ osim.m
@@ -1,7 +1,7 @@
 function [ m1, m2, m3, mtp, awt ] = osim( tmax, til, run )
 %OSIM Packaging optimization
 %   tmax - simulation time
-%   til - tiling in seconds
+%   til - til in seconds
 %   run - number of runs
     awt = NaN(100,100,100);
     str = '';
@@ -11,7 +11,7 @@ function [ m1, m2, m3, mtp, awt ] = osim( tmax, til, run )
         error('Incorrect number of runs.');
     end
     if (til<=0 || round(til)~=til )
-        error('Incorrect tiling.');
+        error('Incorrect til.');
     end
     % Optimization loop
     for n = 1:run
@@ -38,4 +38,40 @@ function [ m1, m2, m3, mtp, awt ] = osim( tmax, til, run )
     [mtp,ind] = max(awt(:));
     [m1, m2, m3] = ind2sub(size(awt),ind);
     fprintf('\nMaximum output: %.2f\n\tm1\t%d\n\tm2\t%d\n\tm3\t%d\n',mtp,m1,m2,m3);
+    
+    % Save results
+    save('awt');
+    
+    % Shrink matrix
+    shrunk = zeros(100/til);
+
+    for countx = til:til:100
+        for county = til:til:100
+            for countz = til:til:100
+                shrunk(countx/til, county/til, countz/til) = awt(countx, county, countz);
+            end
+        end
+    end
+
+    % Plot results
+    xslice = [m1];
+    yslice = [m2];
+    zslice = [m3];
+
+    x = til:til:100;
+    y = til:til:100;
+    z = til:til:100;
+    
+    % Display the slices
+    slice(x, y, z, shrunk, xslice, yslice, zslice)    
+    ylim([-5 105])
+    view(135, 45)
+    xlabel('r1');
+    ylabel('r2');
+    zlabel('r3');
+    title(mtp);
+    
+    % Create and label the colorbar
+    cb = colorbar;
+    cb.Label.String = 'Packages per hour';
 end
